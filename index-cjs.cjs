918f04b00375208653097581f38ca12af6795bc3
Srijan67 <82119082+Srijan67@users.noreply.github.com>
2024-12-04 01:24:58 +0530

    Foreground windows support for win32 platform

---
+++ index-cjs.cjs
@@ -0,0 +1,86 @@
+const path = require('path')
+const {EventEmitter} = require('events');
+const {promisify} = require('util')
+const { spawn, execFile } = require('child_process');
+const foregroundListener = new EventEmitter();
+const execAsync = promisify(execFile)
+let child = null
+if(process.platform !== 'win32'){
+    console.error("This platform is not supported right now by the package!"); 
+    process.exit(1)
+}
+const exefile= path.join(__dirname, "lib", "foreground.exe")
+async function getForegroundWindow() {
+    try {
+        const {stdout, stderr} = await execAsync(exefile); 
+        if(stdout && stdout.length > 0){
+            const output = stdout.toString(); 
+            const outputSplit = output.split('\r\n')
+            let processName = outputSplit[2].split("Process Name: ")[1]
+            let processId = outputSplit[1].split('Process ID: ')[1]
+            let winTitle = outputSplit[0].split('Foreground Window Title: ')[1]
+            let obj = {
+                processId: processId,
+                processName: processName,
+                windowTitle: winTitle
+            }
+            return obj;
+        }else if(stderr){
+            return stderr.toString()
+        }
+    } catch (error) {
+        console.log('Error: ', error)
+        return error
+        
+    }
+  
+}
+function startListening() { 
+    if (child) { 
+        console.log('Listener is already running.'); 
+        return; 
+    } 
+    console.log('Starting foreground listener...'); 
+    child = spawn(exefile, ['event']); 
+    child.stdout.on('data', (data) => { 
+        const output = data.toString(); 
+        const outputSplit = output.split('\r\n')
+        if(outputSplit.length > 2){
+
+            let processName = outputSplit[2].split("Process Name: ")[1]
+            let processId = outputSplit[1].split('Process ID: ')[1]
+            let winTitle = outputSplit[0].split('Foreground Window Title: ')[1]
+            let obj = {
+                processId: processId,
+                processName: processName,
+                windowTitle: winTitle
+            }
+            
+            foregroundListener.emit('change', obj); 
+        }else {
+            foregroundListener.emit('change', output); 
+
+        }
+    }); 
+    child.stderr.on('data', (data) => { 
+        const output = data.toString(); 
+        console.error(`stderr: ${output}`); 
+    }); 
+    child.on('exit', (code) => { 
+        console.log(`Foreground listener exited with code ${code}`); 
+        child = null; 
+    });
+}
+function stopListening() { 
+    if (child) { 
+        child.kill(); 
+        child = null; 
+        console.log('Stopped foreground listener.'); 
+    } else { 
+        console.log('Listener is not running.'); 
+    }
+}
+const foreground = {
+    startListening, stopListening, on: (event, listener) => foregroundListener.on(event, listener), getForegroundWindow
+}
+module.exports = foreground;
---
+++ lib/foreground.c
@@ -0,0 +1,65 @@
+#include <windows.h>
+#include <psapi.h>
+#include <stdio.h>
+#include <time.h>
+#define MAX_TITLE_LENGTH 256
+
+void GetForegroundWindowDetails(char* details, size_t size) {
+    HWND hwnd = GetForegroundWindow();
+    if (hwnd) {
+        char title[MAX_TITLE_LENGTH];
+        DWORD processID;
+        GetWindowTextA(hwnd, title, sizeof(title));
+        GetWindowThreadProcessId(hwnd, &processID);
+        HANDLE hProcess = OpenProcess(PROCESS_QUERY_INFORMATION | PROCESS_VM_READ, FALSE, processID); 
+        char processName[MAX_PATH] = ""; 
+        if (hProcess) { 
+            HMODULE hMod; 
+            DWORD cbNeeded; 
+            if (EnumProcessModules(hProcess, &hMod, sizeof(hMod), &cbNeeded)) { 
+                GetModuleBaseNameA(hProcess, hMod, processName, sizeof(processName) / sizeof(char)); 
+                } 
+                CloseHandle(hProcess); 
+            }
+        snprintf(details, size, "Foreground Window Title: %s\nProcess ID: %lu\nProcess Name: %s\n", title, processID, processName); 
+        printf("%s", details);
+        fflush(stdout);
+
+    }
+
+}
+
+void CALLBACK EventProc(HWINEVENTHOOK hook, DWORD event, HWND hwnd, LONG idObject, LONG idChild, DWORD dwEventThread, DWORD dwmsEventTime) {
+    if (event == EVENT_SYSTEM_FOREGROUND) {
+        char details[512] = { 0 };
+        GetForegroundWindowDetails(details, sizeof(details)); 
+
+    }
+}
+
+int main(int argc, char* argv[]) {
+ if (argc > 1 && strcmp(argv[1], "event") == 0) {
+    HWINEVENTHOOK hook = SetWinEventHook(
+        EVENT_SYSTEM_FOREGROUND, EVENT_SYSTEM_FOREGROUND,
+        NULL, EventProc, 0, 0, WINEVENT_OUTOFCONTEXT
+    );
+
+    if (hook == NULL) {
+        printf("Failed to set event hook.\n");
+        return 1;
+    }
+
+    printf("Monitoring foreground window changes... Press Ctrl+C to exit.\n");
+    fflush(stdout);
+    MSG msg;
+    while (GetMessage(&msg, NULL, 0, 0)) {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+    UnhookWinEvent(hook);
+    return 0;
+ }else {
+    char details[512] = { 0 }; 
+    GetForegroundWindowDetails(details, sizeof(details));
+ }
+}
